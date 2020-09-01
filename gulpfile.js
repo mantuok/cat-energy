59197b2f1e989bb7daa7dac434f7e1079c72400b
mantuok <mantuok@gmail.com>
2020-09-02 00:26:31 +0300

    Автоматизация сборки проекта

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,40 @@
 const gulp = require("gulp");
+const del = require("del");
 const plumber = require("gulp-plumber");
 const sourcemap = require("gulp-sourcemaps");
 const less = require("gulp-less");
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
+const csso = require("gulp-csso");
+const rename = require("gulp-rename");
+const imagemin = require("gulp-imagemin");
+const webp = require("gulp-webp");
+const svgstore = require("gulp-svgstore");
 const sync = require("browser-sync").create();
+
+// Clean
+
+const clean = () => {
+  return del("build");
+};
+
+exports.clean = clean;
+
+// Copy
+
+const copy = () => {
+  return gulp.src ([
+    "source/fonts/**/*.{woff,woff2}",
+    "source/img/**",
+    "source/js/**",
+    "source/*.html"
+  ], {
+    base: "source"
+  })
+    .pipe(gulp.dest("build"));
+};
+
+exports.copy = copy;
 
 // Styles
 
@@ -16,19 +46,55 @@ const styles = () => {
     .pipe(postcss([
       autoprefixer()
     ]))
+    .pipe(csso())
+    .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(sync.stream());
 }
 
 exports.styles = styles;
+
+// Image Optimization
+
+const images = () => {
+  return gulp.src("biuld/img/**/*.{jpg,png,svg}")
+    .pipe(imagemin ([
+      imagemin.mozjpeg({progressive: true}),
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.svgo()
+    ]))
+}
+
+exports.images = images;
+
+// WebP Creation
+
+const createWebp = () => {
+  return gulp.src("build/img/**/*.{jpg,png}")
+    .pipe(webp({quality: 80}))
+    .pipe(gulp.dest("build/img"))
+}
+
+exports.webp = createWebp;
+
+// Sprite Creation
+
+const sprite = () => {
+  return gulp.src("build/img/**/icon-*.svg")
+    .pipe(svgstore())
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("build/img"))
+}
+
+exports.sprite = sprite;
 
 // Server
 
 const server = (done) => {
   sync.init({
     server: {
-      baseDir: 'source'
+      baseDir: 'build'
     },
     cors: true,
     notify: false,
@@ -46,6 +112,7 @@ const watcher = () => {
   gulp.watch("source/*.html").on("change", sync.reload);
 }
 
-exports.default = gulp.series(
-  styles, server, watcher
-);
+const build = gulp.series(clean, copy, styles, images, createWebp, sprite);
+
+exports.build = build;
+
